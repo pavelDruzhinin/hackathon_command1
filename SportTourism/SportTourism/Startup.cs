e45dfded73e77c471a92fcd06dd4db3adf9119a3
SmirnovNV <smirnov_work@mail.ru>
2016-11-27 18:30:05 +0300

    Перезалил проект. Таблицу Users еще буду редактировать.

---
+++ SportTourism/SportTourism/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportTourism.Startup))]
+namespace SportTourism
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
