f2c042734e1e5943f9c3c69bd47a7a661a285f22
SmirnovNV <smirnov_work@mail.ru>
2016-11-27 15:56:28 +0300

    Первый вариант. С моделью user.

---
+++ SportTourism/SportTourism/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportTourism.MvcApplication" Language="C#" %>
---
+++ SportTourism/SportTourism/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportTourism.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime Birthday { get; set; }
+        public string MyProperty { get; set; }
+        public string ContactTelEMail { get; set; }
+        public bool IsSportsman { get; set; }
+        public string Gender { get; set; }
+        public string Skills { get; set; }
+        public int OrderID { get; set; }
+
+    }
+}
