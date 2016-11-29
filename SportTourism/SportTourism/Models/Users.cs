5d5c33069462b7f7ee1bf804f36fc0297250036a
SmirnovNV <smirnov_work@mail.ru>
2016-11-29 07:35:41 +0300

    Сделал модель Users

--- SportTourism/SportTourism/Models/Users.cs
+++ SportTourism/SportTourism/Models/Users.cs
@@ -8,13 +8,18 @@ namespace SportTourism.Models
     public class Users
     {
         public int Id { get; set; }
-        public string FIO { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
         public DateTime Birthday { get; set; }
-        public string MyProperty { get; set; }
-        public string ContactTelEMail { get; set; }
-        public bool IsSportsman { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string AboutMe { get; set; }
         public string Gender { get; set; }
-        public string Skills { get; set; }
-        public int OrderID { get; set; }
+        public SportLevel SportLevel { get; set; } //много user -> один SportLevel
+        public List<Skills> Skills { get; set; } //один user -> много Skills
+
+        public List<OrganizationsUsers> OrganizationsUsers { get; set; } //?!?!?!? один user -> много OrganizationsUsers
+        public List<EventsUsers> EventsUsers { get; set; } //?!?!? один user -> много EventsUsers 
     }
 }
