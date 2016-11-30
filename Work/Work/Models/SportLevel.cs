d1f0339c56567a5b98fa88ed00e733713d184dc6
DESKTOP-54SSP0T\Chelovek <good_morning_06@mail.ru>
2016-12-01 01:20:28 +0300

    Добавил модели Skill, SkillUsers (для таблицы SkillsUsers), SportLevel. Добавил ключи для этих таблиц в User

---
+++ Work/Work/Models/SportLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Work.Models
+{
+    public class SportLevel
+    {
+        public int Id { get; set; }
+        public string Level { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
