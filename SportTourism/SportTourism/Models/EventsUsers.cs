2688abd5056c15962e4f41babdf401c5137d37a3
Egorsergeevichw <yodaskillme@gmail.com>
2016-11-30 13:18:36 +0300

    EventsUsers
    Добавил EventsUsers

---
+++ SportTourism/SportTourism/Models/EventsUsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportTourism.Models
+{
+    public class EventsUsers
+    {
+        // связь один ко многим
+        public Users User { get; set; }
+        public Events Event { get; set; }
+
+        // поля для хранения Id (будут использованы как составной ключ)
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+    }
+}
